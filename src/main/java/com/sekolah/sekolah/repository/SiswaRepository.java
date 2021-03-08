f6944b9df0fabfefdee09c9c9f1f4a509d21b62a
andikscript <andikscript@gmail.com>
2021-03-08 09:59:48 +0700

    add comment

--- src/main/java/com/sekolah/sekolah/repository/SiswaRepository.java
+++ src/main/java/com/sekolah/sekolah/repository/SiswaRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SiswaRepository extends JpaRepository<Siswa, String> {
+    // list dibawah ini dibuat secara manual dengan mengacu pada attribut pada model
     List<Siswa> findByNama(String nama);
     List<Siswa> findByAlamat(String alamat);
 }
