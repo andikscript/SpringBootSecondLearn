d8670b5649115d028f728965fff64e8e5cc3a325
andikscript <andikscript@gmail.com>
2021-03-11 20:04:13 +0700

    add method pencarian pengganti keyword LIKE

--- src/main/java/com/sekolah/sekolah/repository/SiswaRepository.java
+++ src/main/java/com/sekolah/sekolah/repository/SiswaRepository.java
@@ -2,6 +2,7 @@ package com.sekolah.sekolah.repository;
 
 import com.sekolah.sekolah.model.Siswa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,4 +10,25 @@ public interface SiswaRepository extends JpaRepository<Siswa, String> {
     // list dibawah ini dibuat secara manual dengan mengacu pada attribut pada model
     List<Siswa> findByNama(String nama);
     List<Siswa> findByAlamat(String alamat);
+
+    // tambahan kata containing fungsinya sama seperti keyword LIKE di sql
+    List<Siswa> findByNamaContaining(String nama);
+    List<Siswa> findByAlamatContaining(String alamat);
+
+    // tambahan kata StartsWith fungsinya untuk mencari kalimat yang awalannya diinputkan oleh user
+    List<Siswa> findByNamaStartsWith(String nama);
+    List<Siswa> findByAlamatStartsWith(String alamat);
+
+    // tambahan kata EndsWith fungsinya untuk mencari kalimat yang akhirannya diinputkan oleh user
+    List<Siswa> findByNamaEndsWith(String nama);
+    List<Siswa> findByAlamatEndsWith(String alamat);
+
+    // tambahan kata NotContaining kebalikan dari Containing yang artinya jika diinputkan adalah sebuah
+    // kalimat nama maka nama tersebut tidak akan tertampil, yang tertampil hanyalah nama yang tidak mengandung nama
+    // yang di inputkan
+    List<Siswa> findByNamaNotContaining(String nama);
+    List<Siswa> findByAlamatNotContaining(String alamat);
+
+    //
+
 }
